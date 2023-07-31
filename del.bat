80def1452e5470b6c8130f5126dc7c92f711ec28
sasiqwerty-id3 <sarathsasi.g@gmail.com>
2023-07-31 10:18:18 +0530

    chatgpt script to remove files - sri

---
+++ del.bat
@@ -0,0 +1,34 @@
+# Set the folder names you want to search for and delete (separated by commas)
+$targetFolderNames = "applications,dailynote,openai,podcasts,powershell,youtube,obsidian"
+
+# Convert the target folder names to an array
+$targetFolders = $targetFolderNames -split ','
+
+# Get a list of all folders with the specified names in the current directory and its subdirectories
+$foldersToDelete = Get-ChildItem -Path "C:\Users\sarat\OneDrive\Desktop\ObsExport" -Recurse |
+    Where-Object { $_.PSIsContainer -and $targetFolders -contains $_.Name }
+
+# Check if any matching folders were found
+if ($foldersToDelete.Count -eq 0) {
+    Write-Host "No folders with the specified names found."
+}
+else {
+    Write-Host "Folders with the specified names found:"
+
+    # Print the names of the matching folders
+    foreach ($folder in $foldersToDelete) {
+        Write-Host $folder.FullName
+    }
+
+    # Ask for confirmation before deleting the folders
+    $confirmation = Read-Host "Do you want to delete these folders? (Y/N)"
+    if ($confirmation -eq "Y" -or $confirmation -eq "y") {
+        # Delete the folders and their contents
+        $foldersToDelete | Remove-Item -Recurse -Force
+        Write-Host "Folders deleted successfully."
+    }
+    else {
+        Write-Host "Folders were not deleted."
+    }
+}
+pause
